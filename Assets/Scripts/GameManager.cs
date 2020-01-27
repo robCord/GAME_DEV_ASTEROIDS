1add1e16f6e160806b506c90ba09c0825fdc292d
Roberto <rob_cord@aol.com>
2020-01-27 12:13:24 -0700

    Added changes from class.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int Score = 0;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this; 
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("GameObject has been destroyed, tried to create a second game manager");
+        }
+    }
+}
